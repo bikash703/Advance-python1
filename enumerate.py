44059d61aa6a5de08c561bd50874b151aed8f71a
Bikash Majhi <98630071+bikash703@users.noreply.github.com>
2022-02-02 11:43:02 +0530

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,7 @@
+l1=[56, 76, 9, 4, 5, 6, 27, 86]
+# index=0
+# for item in l1:
+#     print(item,index)
+#     index +=1
+for index,item in enumerate(l1):
+    print(item,index)
---
+++ multiplication-table-using-list_comprehension.py
@@ -0,0 +1,3 @@
+a=int(input('Enter a Number: '))
+l2=[a*i for i in range(1,11)]
+print(l2)
